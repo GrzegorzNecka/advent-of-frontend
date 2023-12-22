0993c068cd7723cb18aedeff1b0c7fb3efb5c376
GrzegorzNecka <global-strzypior@gmail.com>
2023-12-22 14:41:11 +0100

    artykuł o grafach

---
+++ tasks/2023-12-10/index.ts
@@ -0,0 +1,9 @@
+//http://jsdn.pl/javascript-noob-struktury-danych-graf/
+//https://swistak.codes/post/grafy-wprowadzenie/
+//https://swistak.codes/post/przechodzenie-po-grafie/
+
+export function findCyclesBetweenLocations(graph: Record<string, string[]>): string[][] {
+    const node = graph[0];
+
+    throw new Error("Function not implemented.");
+}
